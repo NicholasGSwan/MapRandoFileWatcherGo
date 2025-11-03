2b14ad6d1b3af2c8621ad69727c7fa7c4173c1f0
Nicholas Swan <nicholasgswan@gmail.com>
2025-11-03 01:49:15 -0800

    moved logic to run func, removed hardcoded directories to use config file instead

--- main.go
+++ main.go
@@ -6,18 +6,25 @@ import (
 	"regexp"
 	"strings"
 
+	"github.com/namsral/flag"
+
 	"github.com/fsnotify/fsnotify"
 )
 
 const (
-	downloadsDir      string = "E:/downloads"
-	mapRandosDir      string = "E:/downloads/maprandos"
 	mapRandoFileRegex string = "(map-rando-)([a-zA-Z0-9]+)(.sfc)"
 )
 
 func main() {
+	conf := &config{}
+	run(conf)
+
+}
+
+func run(conf *config) error {
 	watcher, err := fsnotify.NewWatcher()
 	regex, err2 := regexp.Compile(mapRandoFileRegex)
+
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -26,6 +33,13 @@ func main() {
 	}
 	defer watcher.Close()
 
+	//initialize config from commandline args
+	conf.init(os.Args)
+	//create maprando directory if not exists
+	err = os.MkdirAll(conf.mapRandosDir, os.ModePerm)
+	if err != nil {
+		log.Fatal(err)
+	}
 	// Start listening for events.
 	go func() {
 		for {
@@ -40,7 +54,7 @@ func main() {
 					fName := strArr[len(strArr)-1]
 					log.Println("modified file:", event.Name)
 					log.Println("file without path is:", fName)
-					err3 := os.Rename(downloadsDir+"\\"+fName, mapRandosDir+"\\"+fName)
+					err3 := os.Rename(conf.downloadsDir+"\\"+fName, conf.mapRandosDir+"\\"+fName)
 					if err3 != nil {
 						log.Fatal(err3)
 					}
@@ -54,12 +68,35 @@ func main() {
 		}
 	}()
 
-	err = watcher.Add(downloadsDir)
+	err = watcher.Add(conf.downloadsDir)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	// Block main goroutine forever.
 	<-make(chan struct{})
+	return nil
+}
 
+type config struct {
+	downloadsDir string
+	mapRandosDir string
+}
+
+func (c *config) init(args []string) error {
+	flags := flag.NewFlagSet(args[0], flag.ExitOnError)
+	flags.String(flag.DefaultConfigFlagname, "", "Path to config file")
+
+	var (
+		downloadsDir = flags.String("downloadsDir", "", "The downloads folder to watch for new map rando roms")
+		mapRandosDir = flags.String("mapRandosDir", "", "The folder to move the map rando roms to")
+	)
+
+	if err := flags.Parse(args[1:]); err != nil {
+		return err
+	}
+
+	c.downloadsDir = *downloadsDir
+	c.mapRandosDir = *mapRandosDir
+
+	return nil
 }
